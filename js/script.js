41e13f88039c5afa9bc9b7dbfc735c45c5994d84
chris-grant-rosterfy <chris-grant-rosterfy@users.noreply.github.com>
2017-04-07 14:23:14 +1000

    send to technology contact
    send to technology contact form

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ $(function () {
 
     $('#contact-form').on('submit', function (e) {
         if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
+            var url = "technology/contact.php";
 
             $.ajax({
                 type: "POST",
