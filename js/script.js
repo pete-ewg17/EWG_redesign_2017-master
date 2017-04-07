df00ad2cc4484c78257d1f19b6d6306b9d2e69b2
chris-grant-rosterfy <chris-grant-rosterfy@users.noreply.github.com>
2017-04-07 14:32:47 +1000

    alert user of submission
    alert user of submission

--- js/script.js
+++ js/script.js
@@ -53,14 +53,7 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                    }
+                    alert("Thank you");
                 }
             });
             return false;
